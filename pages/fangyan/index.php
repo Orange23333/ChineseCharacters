fd96308d6e029780b73294b3b71187782945fcd4
Orange_233 <874462262@qq.com>
2022-10-02 00:12:10 +0800

    Update License

--- pages/fangyan/index.php
+++ pages/fangyan/index.php
@@ -50,7 +50,7 @@ EOF, "class=\"fy-text-p\"");
 			$bottomInfo = nl2p(<<<EOF
 版权所有 2022 全部的原作者和贡献者
 Copyright (c) 2022 All Original Authors and Contributors
-<a href="https://gitee.com/orange23333/chinese-characters/blob/main/LICENSE.zh-CN.txt">使用许可证（简体中文）</a> | <a href="https://github.com/Orange23333/ChineseCharacters/blob/main/LICENSE.en-US.txt">License (English)</a>
+<a href="https://gitee.com/orange23333/chinese-characters/blob/main/LICENSE.txt">使用许可证（简体中文）</a> | <a href="https://github.com/Orange23333/ChineseCharacters/blob/main/LICENSE.en-US.txt">License (English)</a>
 <a href="https://gitee.com/orange23333/chinese-characters">码云 Gitee</a> | <a href="https://github.com/Orange23333/ChineseCharacters">Github</a>
 EOF, "class=\"os-bottom-info-p\"");
 
