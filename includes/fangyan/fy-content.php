6d85bb2717b8a8997744ac733d0829da0e4dad7a
Orange_233 <874462262@qq.com>
2023-01-18 05:01:42 +0800

    * Correct license title in English, so the version number license in both language has been changed. * Correct punctuation marks: Use `〔〕` insteads `[]` for dynasty as original book. * Supplement the missing sign off. + Add book info. (I'm too lazy to think off what is need, so I write down them all.) * Update `fangyan.css`.

--- includes/fangyan/fy-content.php
+++ includes/fangyan/fy-content.php
@@ -1,14 +1,44 @@
 <?php
-$fy_title = '輶轩使者绝代语释别国方言';
-$fy_author = '[汉] 杨雄 撰 | [晋] 郭璞 注';
+$fy_front_cover_title = '輶轩使者绝代语释别国方言';
+$fy_front_cover_author = <<<EOF
+〔汉〕杨雄 撰 | 〔晋〕郭璞 注
+<a href="http://www.zhbc.com.cn">中华书局</a>
+EOF;
+$fy_front_cover_notes = '附 音序、笔画 索引';
+$fy_book_info_title = '<a id="FangYan_BookInfo">图书在版编目（CIP）数据</a>';
+$fy_book_info_text = <<<EOF
+方言:附音序、笔画索引/（汉）杨雄撰;（晋）郭璞注. —北京:中华书局,2016.6(2019.11重印)
+ISBN 978-7-101-11746-2
+Ⅰ.方…  Ⅱ.①杨…②郭…  Ⅲ.汉语方言–古方言–研究 Ⅳ.H171
+中国版本图书馆CIP数据核字(2016)第087117号
+<a style="font-size: 11pt;">责任编辑：张可</a>
+<a style="font-size: 15pt; font-weight: bold;">方言</a><a style="font-size: 11pt;">附音序、笔画索引</a>
+<a style="font-size: 11pt;">〔汉〕杨雄 撰</a>
+<a style="font-size: 11pt;">〔晋〕郭璞 注</a>
+<a style="font-size: 12pt;">中华书局出版发行</a>
+<a style="font-size: 10pt;">（北京市丰台区太平桥西里38号  100073）</a>
+<a style="font-size: 11pt;">http://www.zhbc.com.cn</a>
+<a style="font-size: 11pt;">E-mail:zhbc@zhbc.com.cn</a>
+<a style="font-size: 12pt;">北京瑞古冠中印厂印刷</a>
+<a style="font-size: 10pt;">880×1230毫米1/32·7¾印张·2插页</a>
+<a style="font-size: 10pt;">2016年6月北京第1版    2019年11月北京第4次印刷</a>
+<a style="font-size: 10pt;">印数：9001-12000册    定价：24.00元</a>
+---
+<a style="font-size: 12pt;">ISBN 978-7-101-11746-2</a>
+EOF;
 $fy_publication_instructions_title = '<a id="FangYan_PublicationInstructions">出版说明</a>';
 $fy_publication_instructions = <<<EOF
 我国传统的语言文字学又称小学，为治经明史之基础，其发展源远流长，经历了先秦的萌芽时期，到汉代出现了重要的语言学著作《尔雅》《方言》《说文解字》和《释名》。作为传统语言学的奠基之作，这四部典籍为历代治学者所重视，为中华文化的传承作出了重要贡献。当下传统文化深受重视，许多读者由文而史，而经，进而对上述四种小学典籍产生了很大兴趣，却苦于找不到一种既有一定权威性，又便于使用的本子。有鉴于此，我们不以所谓善本、孤本为追求目标，而选取四种经典版本加以影印，施以圈点句读、标示字头、编制索引，为学界提供方便精善的读本。
 《方言》，原名《輶轩使者绝代语释别国方言》，旧题西汉扬雄撰，是我国第一部汉语方言比较词汇集。东晋郭璞为《方言》作注，不仅校字注音、疏证词义、阐明体例，更以晋时方言来与杨雄所记录的汉时相比较，从而揭示了三百年间汉语方言的历时变化，也呈现了晋代方言的一些实际面貌。
 《方言》宋代曾有国子监本、蜀本、闽本；宋庆元庚申（一二〇〇）会稽李梦傅浔阳郡斋刻本盖即蜀本之覆刻本。该本为宋椠孤本，现存之明刊本、抄本均祖于该本，仅文字有少许差异。今日所见之李梦傅刻本递经明、清名家收藏，至民国为傅增湘购藏重装，于卷首增重装题签，卷末收民国缪荃孙、沈增植等人题跋。今以宋庆元六年（一二〇〇）会稽李梦傅浔阳郡斋刻本为底本印行，傅增湘重装时增题跋一并保留。底本残缺严重者，参以他本，以编者注形式补于书后。索引所涉字，凡讹不成字者，及虽见于子书然与郭注音义不合或与上下文义殊违者，为便检索，酌情出注；既见于字书，于郭注音义上下文义亦无明显违逆者，虽已经前贤考证为讹字，但因不属于本次整理范畴，鼓不再出注。编辑过程中参考了华学诚先生的《杨雄方言校释汇证》，特予说明，并致谢訦。不到之处，希望读者随时指正。
 EOF;
+$fy_publication_instructions_sign_off = <<<EOF
+中华书局编辑部
+二〇一六年一月
+EOF;
 $fy_menu_title = '目录';
 $fy_menu = <<<EOF
+<a href="#FangYan_BookInfo">图书在版编目（CIP）数据</a>
 <a href="#FangYan_PublicationInstructions">出版说明</a>
 <a href="#FangYan_Preface">方言序</a>
 EOF;
@@ -16,15 +46,19 @@ $fy_preface_title = '<a id="FangYan_Preface">方言序</a>';
 $fy_preface_author = "郭璞";
 $fy_preface = <<<EOF
 <i>（标点是自行修改的，不一定正确。用简体只是为了方便，可能繁体更接近原有意思，标有*号（*号左侧的）字是找不到字符的替代，不一定正确）</i>
-盖闻方言之作，出乎輶轩之使。所以巡游万国，采览异言。车轨之所交，人际之所蹈。靡不毕载，以为奏籍。周秦之季，其业隳废，莫有存者，曁乎扬生，沉淡其志，历载构缀，乃就斯文。是以三五之篇著，而独鉴之功显。故可不出户庭而坐照四表，不劳畴咨而物来能名。考九服之逸言，摽六代之绝语。类离词之指韵*（……）
+盖闻方言之作，出乎輶轩之使。所以巡游万国，采览异言。车轨之所交，人际之所蹈。靡不毕载，以为奏籍。周秦之季，其业隳废，莫有存者，曁乎扬生，沉淡其志，历载构缀，乃就斯文。是以三五之篇著，而独鉴之功显。故可不出户庭而坐照四表，不劳畴咨而物来能名。考九服之逸言，摽六代之绝语。类离词之指韵*（……正在更新）
 EOF;
 
 // array(array(string $htmlLabelClassName, string $text))
 $fy_content = array(
-    array('fy-title', $fy_title),
-    array('fy-book-info', $fy_author),
+    array('fy-title', $fy_front_cover_title),
+    array('fy-book-info', $fy_front_cover_author),
+    array('fy-book-info', $fy_front_cover_notes),
+	array('fy-sub-title', $fy_book_info_title),
+	array('fy-text', $fy_book_info_text),
 	array('fy-sub-title', $fy_publication_instructions_title),
 	array('fy-text', $fy_publication_instructions),
+	array('fy-sign-off', $fy_publication_instructions_sign_off),
 	array('fy-sub-title', $fy_menu_title),
 	array('fy-text', $fy_menu),
     array('fy-sub-title', $fy_preface_title),
